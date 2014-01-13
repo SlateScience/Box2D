618b408b7016cd759384fb5a5ae7c94248ef596d
Michael Boccara <michael@slatescience.com>
2014-01-13 23:31:15 +0200

    Imported makefile for static instead of shared lib

--- android/Android.mk
+++ android/Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := Box2D
-LOCAL_SRC_FILES := ../lib/android/$(TARGET_ARCH_ABI)/libBox2D.so
+LOCAL_SRC_FILES := ../lib/android/$(TARGET_ARCH_ABI)/libBox2D.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../src
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
